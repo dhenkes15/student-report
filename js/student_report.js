c7d89afa966bd987c254b0725ec6787b0229e1c4
Daniel H <dhenkes95@icloud.com>
2019-10-08 16:50:22 +0000

    Added prompt for name

--- js/student_report.js
+++ js/student_report.js
@@ -1,10 +1,22 @@
-for (i = 0; i < students.length; i += 1) {
-   var name = students[i].name;
-   var track = students[i].track;
-   var achievements = students[i].achievements;
-   var points = students[i].points;
-  document.write("<h2> Name: " + name + "</h2>");
-  document.write("<p> Track: " + track + "</p>");
-  document.write("<p> Achievements: " + achievements + "</p>");
-  document.write("<p> Points: " + points + "</p>"); 
+
+while (true) {
+  var response = prompt("Type a name to receive the student's report card or quit to exit");
+  response.toLowerCase();
+  if ( response === quit ) {
+    break;
+  } else {
+    for (i = 0; i < students.length; i += 1) {
+    var name = students[i].name;
+    name.toLowerCase();
+    var track = students[i].track;
+    var achievements = students[i].achievements;
+    var points = students[i].points;
+      if (name === response) {
+        document.write("<h2> Name: " + students[i].name + "</h2>");
+        document.write("<p> Track: " + track + "</p>");
+        document.write("<p> Achievements: " + achievements + "</p>");
+        document.write("<p> Points: " + points + "</p>");
+      }
+    }
+  }
 }
